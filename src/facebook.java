1c3005523f0d08baf48c49b06fece4aa06d79538
Joao Victtor S Melo <joaovicttorsilveiramelo@gmail.com>
2020-08-31 15:11:56 -0300

    Arrumei espaços em branco e arrumei o plural das palavras

--- src/facebook.java
+++ src/facebook.java
@@ -4,19 +4,15 @@ public class facebook {
 		for (int i = 1; i <= 100; i++) {
 			facebook.teste(i);
 		}
-
 	}
 
 	public static void teste(int num) {
 
 		boolean cont = (num % 2 == 0) ? true : false;
 		if (cont == true) {
-			System.out.println("your number " + num + ": evens ");
-
+			System.out.println("your number " + num + ": even");
 		} else {
-			System.out.println("your number " + num + ": odds ");
+			System.out.println("your number " + num + ": odd");
 		}
-
 	}
-
 }
