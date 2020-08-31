1b842498b973bf185d9c4039360a6d2366ba3779
Joao Victtor S Melo <joaovicttorsilveiramelo@gmail.com>
2020-08-31 00:54:38 -0300

    Recriei um meme do facebook na maneira mais simples possivel

---
+++ src/project/facebook.java
@@ -0,0 +1,24 @@
+package project;
+
+public class facebook {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			facebook.teste(i);
+		}
+
+	}
+
+	public static void teste(int num) {
+
+		boolean cont = (num % 2 == 0) ? true : false;
+		if (cont == true) {
+			System.out.println("your number " + num + ": even ");
+
+		} else {
+			System.out.println("your number " + num + ": odd ");
+		}
+
+	}
+
+}
